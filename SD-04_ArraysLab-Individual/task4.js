1729301ea0708cf52eedff89119ea5f500400a40
Daniela Magaña <daniela.98mg@gmail.com>
2026-09-25 20:20:37 -0600

    Añadi un comentario en task4.js

--- SD-04_ArraysLab-Individual/task4.js
+++ SD-04_ArraysLab-Individual/task4.js
@@ -2,7 +2,7 @@ const arr = [1,2,3,4,5,5,6,7,8,9,10,11,12,13,14]
 
 // Type your code below this line!
 
-arr.splice(5,1)
+arr.splice(5,1)  //1 indicaa eliminar
 
 console.log("--- Splice (deleting) ---")
 // Type your code above this line!
